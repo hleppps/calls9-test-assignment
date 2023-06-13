2de78ba9bca5b7369bff2408707a734b8c4b5898
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-14 00:56:37 +0300

    fix: add max length for comment text field

--- src/components/unsorted/CommentForm/validationSchema.ts
+++ src/components/unsorted/CommentForm/validationSchema.ts
@@ -5,7 +5,7 @@ export const validationSchema = yup.object({
   text: yup
     .string()
     .min(3, 'Comment text should be of minimum 3 character length')
-    .min(100, 'Comment text should not be longer than 100 characters')
+    .max(100, 'Comment text should not be longer than 100 characters')
     .required('Comment text is required'),
   storyId: yup.number().required('Story is required'),
 });

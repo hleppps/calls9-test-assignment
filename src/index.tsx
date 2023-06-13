1fb02562a9d2b68663a1cb05fd3c70459c9d1b86
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-13 16:40:34 +0300

    feat: move App component into corresponding folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-import './index.css';
-
+import App from 'components/app/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,

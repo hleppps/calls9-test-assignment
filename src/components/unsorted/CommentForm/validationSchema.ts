b4c3dcdc6b4285c63936822a6148b38eeb2368c6
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-14 00:05:03 +0300

    feat: change texts for warning messages in comment

--- src/components/unsorted/CommentForm/validationSchema.ts
+++ src/components/unsorted/CommentForm/validationSchema.ts
@@ -4,7 +4,7 @@ export const validationSchema = yup.object({
   name: yup.string().max(40).required('Name is required'),
   text: yup
     .string()
-    .min(3, 'Text should be of minimum 3 character length')
-    .required('Text is required'),
+    .min(3, 'Comment text should be of minimum 3 character length')
+    .required('Comment text is required'),
   storyId: yup.number().required('Story is required'),
 });

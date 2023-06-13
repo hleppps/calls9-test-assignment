00a7f05ce1ab78d894ec4029fec3edbaf3900010
Hlieb Shchehelskyi <hlieb.shchehelskyi@vilmate.com>
2023-06-13 22:28:24 +0300

    feat: upload dummy images

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,23 @@
 import { ContentType } from 'utils/constants';
 
 export type Story = {
-  id: string;
+  id: number;
   title: string;
   text: string;
   contentType: ContentType;
   image: { src: string; alt: string };
   tags: string[];
+};
+
+export type Comment = {
+  userName: string;
+  storyId: string;
+  text: string;
+  time: string;
+  type: 'comment';
+};
+
+export type Image = {
+  src: string;
+  alt: string;
 };
---
+++ src/utils/dummyImages.ts
@@ -0,0 +1,12 @@
+import storyImage1 from 'assets/images/story-image-1.jpeg';
+import storyImage2 from 'assets/images/story-image-2.jpeg';
+import storyImage3 from 'assets/images/story-image-3.jpeg';
+import storyImage4 from 'assets/images/story-image-4.jpeg';
+import { Image } from 'types/global';
+
+export const dummyImages: Image[] = [
+  { src: storyImage1, alt: 'Man drawing on the whiteboard' },
+  { src: storyImage2, alt: 'Hand with the smartphone' },
+  { src: storyImage3, alt: 'Man pointing at the monitor' },
+  { src: storyImage4, alt: 'People silhouettes binary code' },
+];
